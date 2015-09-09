4ad2d96dea0b179f1658021ebef2963be20efe11
maksimgm <maksimmamrikov@gmail.com>
2015-09-09 15:06:14 -0700

    add http requests

--- omdb.js
+++ omdb.js
@@ -6,7 +6,22 @@ http.get("http://omdbapi.com/?i=tt0241527",function(res){
   res.setEncoding("utf8");
   res.on("data",function(data){
     arr.push(data);
-    console.log(data);
+    console.log(arr);
+  });
+  // res.on("end",function(){
+  //   console.log("ALL DONE!");
+  // });
+  // res.end();
+});
+
+http.get("http://omdbapi.com/?i=tt0295297",function(res){
+  res.setEncoding("utf8");
+  res.on("data",function(data){
+    arr.push(data);
+    console.log(arr);
+  });
+  res.on("end",function(){
+    console.log("ALL DONE!");
   });
   res.end();
 });
